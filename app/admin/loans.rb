34f2a74850a08a793d31b4c97c3f660f1be8fdfe
Raghu Betina <raghu@firstdraft.com>
2018-11-28 23:14:50 +0000

    Whitelist columns in Loan for ActiveAdmin

--- app/admin/loans.rb
+++ app/admin/loans.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Loan do
+
+ permit_params :current_balance, :original_amount, :interest_rate, :periods_in_year, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
