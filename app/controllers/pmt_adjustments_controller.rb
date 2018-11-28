4e81c276d358d9b4752c461559f2803f4b24c119
Raghu Betina <raghu@firstdraft.com>
2018-11-28 23:15:02 +0000

    Only pmt_adjustment user can access show page

--- app/controllers/pmt_adjustments_controller.rb
+++ app/controllers/pmt_adjustments_controller.rb
@@ -1,5 +1,5 @@
 class PmtAdjustmentsController < ApplicationController
-  before_action :current_user_must_be_pmt_adjustment_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_pmt_adjustment_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_pmt_adjustment_user
     pmt_adjustment = PmtAdjustment.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
