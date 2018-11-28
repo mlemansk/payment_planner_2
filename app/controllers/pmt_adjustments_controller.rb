9356a9e0f86595b6483f21b9835c05fbe1dc9928
Raghu Betina <raghu@firstdraft.com>
2018-11-28 23:15:02 +0000

    Only pmt_adjustment user can modify pmt_adjustment

--- app/controllers/pmt_adjustments_controller.rb
+++ app/controllers/pmt_adjustments_controller.rb
@@ -1,4 +1,14 @@
 class PmtAdjustmentsController < ApplicationController
+  before_action :current_user_must_be_pmt_adjustment_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_pmt_adjustment_user
+    pmt_adjustment = PmtAdjustment.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == pmt_adjustment.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @pmt_adjustments = PmtAdjustment.all
 
